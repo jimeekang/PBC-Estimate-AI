8cfe9bd65f29f47057160bb757c0245811063985
Jimee <pbcjimee@gmail.com>
2026-02-02 10:51:51 +0000

    ReactDOM.useFormState has been renamed to React.useActionState. Please u

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { login } from '@/app/auth/actions';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -20,7 +21,7 @@ function SubmitButton() {
 }
 
 export function LoginForm() {
-  const [state, formAction] = useFormState(login, { errors: {} });
+  const [state, formAction] = useActionState(login, { errors: {} });
 
   return (
     <form action={formAction} className="space-y-6">
